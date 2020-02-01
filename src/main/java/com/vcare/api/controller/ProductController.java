5ffac4cc27d12f4f89cf6376fe40f39b425e885a
Nipun <nipunaggarwal16@gmail.com>
2020-02-01 15:40:31 +0530

    spring mvc changed to spring boot

--- src/main/java/com/vcare/api/controller/ProductController.java
+++ src/main/java/com/vcare/api/controller/ProductController.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +23,7 @@ import com.vcare.api.util.ProductUtil;
  * @author nipunaggarwal
  *
  */
-@RestController
+@Controller
 @RequestMapping(value = {
 		"/product"
 })
